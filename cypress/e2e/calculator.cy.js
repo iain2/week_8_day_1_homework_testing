69fd9ca598c42d6b98e6ea5a3cab91f1937cd7fa
Iain Hay <iainhay@Iains-MacBook-Air.local>
2022-06-20 22:13:59 +0100

    initial commit

---
+++ cypress/e2e/calculator.cy.js
@@ -0,0 +1,64 @@
+describe("Calculator", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  })
+
+  it('should have working number buttons', () => {
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '2')
+  })
+
+  it('Do the arithmetical operations update the display with the result of the operation?', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '4')
+  })
+
+  it('Can multiple operations be chained together?', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '6')
+  })
+
+  it('Is the output as expected for a range of numbers negative numbers)?', () => {
+    cy.get('#number1').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-1')
+  })
+
+  it('Is the output as expected for a range of numbers decimal numbers)?', () => {
+    cy.get('#number1').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '0.5')
+  })
+
+  it('Is the output as expected for a range of numbers decimal numbers)?', () => {
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '998001')
+  })
+
+  it('divide by zero returns error', () => {
+    cy.get('#number2').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', 'error')
+  })
+})
